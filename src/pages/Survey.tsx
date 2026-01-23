958edeea0d79d53b3815b4d7fcb01cc3b0253dae
codyh587 <108317527+codyh587@users.noreply.github.com>
2026-01-22 17:28:27 -0800

    added framer motion and initial survey header

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -1,13 +1,12 @@
-import { ArrowUpIcon } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 
 export function Survey() {
   return (
-    <div className="flex-col">
-      <Button className="block w-full" variant="outline">
-        Button
-      </Button>
+    <div className="h-screen bg-linear-to-br from-red-600 via-yellow-400 to-green-600 flex-col overflow-y-auto p-10 text-center">
+      <div className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance text-white text-shadow-lg">
+        LUCHA SABOR
+      </div>
+      <Button variant="secondary" className="w-full" />
     </div>
   );
 }
