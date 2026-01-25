57281e1e0499c1738339a9ceae68e710ed76eae8
codyh587 <108317527+codyh587@users.noreply.github.com>
2026-01-25 01:46:23 -0800

    Added more fastAPI examples and a bit of installation readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # CHEFCITO
 
 A fancy writeup/product overview will appear here soon
+
+
+## Installation
+1. Install Node.JS
+2. Run `npm i` within the `client` folder
+3. Install Python
+4. Run `python -m venv .venv` within the `server` folder, then also use the venv to install `pip install -r requirements.txt`
+5. Can run `./start_client` and `./start_server` from this main folder to make it easy
--- server/main.py
+++ server/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+# run start_server.bat and go to localhost:8000/docs to test the api
 app = FastAPI()
 
 
+# this is a sample get endpoint
 @app.get("/welcome")
-def read_user():
+def get_welcome():
     return {"message": "Welcome to CHEFCITO!"}
+
+
+# sample endpoint with request body (takes in JSON), this class is required
+# (post endpoints are best when you have a body)
+class EchoBody(BaseModel):
+    field1: str
+    field2: float
+
+
+@app.post("/echo")
+def post_echo(body: EchoBody):
+    return {"field1": body.field1, "field2": body.field2}
+
+
+# please don't write the entire backend in a single file. however,
+# it's more convenient for fastapi to put all the endpoint declarations here.
+# all the helpers/ml stuff can go in different files and these functions
+# can just call those helpers from this file.
