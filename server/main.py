3e32dde1b32349f78c714158f4c53848cf446542
codyh587 <108317527+codyh587@users.noreply.github.com>
2026-02-07 15:47:45 -0800

    clear up fastapi

--- server/main.py
+++ server/main.py
@@ -9,26 +9,8 @@ with open("clean_recipes.jsonl", encoding="utf-8") as f:
         RECIPES.append(json.loads(line))
 RANKER = Ranker()
 
-# run start_server.bat and go to localhost:8000/docs to test the api
+
 app = FastAPI()
-
-
-# this is a sample get endpoint
-@app.get("/welcome")
-def get_welcome():
-    return {"message": "Welcome to CHEFCITO!"}
-
-
-# sample endpoint with request body (takes in JSON), this class is required
-# (post endpoints are best when you have a body)
-class EchoBody(BaseModel):
-    field1: str
-    field2: float
-
-
-@app.post("/echo")
-def post_echo(body: EchoBody):
-    return {"field1": body.field1, "field2": body.field2}
 
 
 class IntentBody(BaseModel):
@@ -47,9 +29,3 @@ class IntentBody(BaseModel):
 def post_recommend(body: IntentBody):
     r = recommend(RECIPES, body.model_dump(), RANKER, [], body.num_reccomendations)
     return r
-
-
-# please don't write the entire backend in a single file. however,
-# it's more convenient for fastapi to put all the endpoint declarations here.
-# all the helpers/ml stuff can go in different files and these functions
-# can just call those helpers from this file.
